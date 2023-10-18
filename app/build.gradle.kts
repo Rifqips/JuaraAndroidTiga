57908305e9f21e9dc5bceaa3b73e447efc925085
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-10-18 14:01:09 +0700

    refactor
    finish excerise race tracker app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    val lifecycle_version = "2.6.2"
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -66,4 +67,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
--- app/src/main/java/id/rifqipadisiliwangi/JuaraAndroidTIga/MainActivity.kt
+++ app/src/main/java/id/rifqipadisiliwangi/JuaraAndroidTIga/MainActivity.kt
@@ -4,43 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import id.rifqipadisiliwangi.JuaraAndroidTIga.ui.theme.JuaraAndroidTIgaTheme
+import id.rifqipadisiliwangi.JuaraAndroidTIga.ui.RaceTrackerApp
+import id.rifqipadisiliwangi.JuaraAndroidTIga.ui.theme.RaceTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JuaraAndroidTIgaTheme {
+            RaceTrackerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RaceTrackerApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JuaraAndroidTIgaTheme {
-        Greeting("Android")
     }
 }
