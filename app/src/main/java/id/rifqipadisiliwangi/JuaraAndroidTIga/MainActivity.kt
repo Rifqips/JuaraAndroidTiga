91ffc1bb4c34189aea2197e3434ad1b193ef2b0e
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-10-18 14:57:15 +0700

    refactor
    finish exercise mars app

--- app/src/main/java/id/rifqipadisiliwangi/JuaraAndroidTIga/MainActivity.kt
+++ app/src/main/java/id/rifqipadisiliwangi/JuaraAndroidTIga/MainActivity.kt
@@ -6,41 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import id.rifqipadisiliwangi.JuaraAndroidTIga.ui.theme.JuaraAndroidTIgaTheme
+import androidx.core.view.WindowCompat
+import id.rifqipadisiliwangi.JuaraAndroidTIga.ui.MarsPhotosApp
+import id.rifqipadisiliwangi.JuaraAndroidTIga.ui.theme.MarsPhotosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            JuaraAndroidTIgaTheme {
+            MarsPhotosTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MarsPhotosApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JuaraAndroidTIgaTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/id/rifqipadisiliwangi/JuaraAndroidTIga/ui/theme/Theme.kt
+++ app/src/main/java/id/rifqipadisiliwangi/JuaraAndroidTIga/ui/theme/Theme.kt
@@ -10,10 +10,12 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -25,23 +27,14 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
-fun JuaraAndroidTIgaTheme(
+fun MarsPhotosTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    // Dynamic color in this app is turned off for learning purposes
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -57,14 +50,15 @@ fun JuaraAndroidTIgaTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
